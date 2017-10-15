552fca42fbceaa048bdc9419763db4c86190ecb1
Jay Asbury <vbjay.net@gmail.com>
2017-10-15 19:12:31 -0400

    Initial project

---
+++ EF Sqlite/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EF Sqlite")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("VBJay Solutions")>
+<Assembly: AssemblyProduct("EF Sqlite")>
+<Assembly: AssemblyCopyright("Copyright © VBJay Solutions 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7d2d5eea-2497-4045-8364-83cdb8d52bd2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>

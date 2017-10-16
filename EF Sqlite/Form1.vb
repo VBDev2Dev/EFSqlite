ac06dad517655acf8024a385c0aeeb563302ca07
Jay Asbury <vbjay.net@gmail.com>
2017-10-15 21:43:48 -0400

    Load data from db

--- EF Sqlite/Form1.vb
+++ EF Sqlite/Form1.vb
@@ -1,3 +1,16 @@
 ﻿Public Class Form1
+﻿Imports System.ComponentModel
+Imports System.Data.Entity
+Public Class Form1
 
+    Dim db As ContactsContext
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        db = New ContactsContext
+        db.Contacts.Load
+        ContactBindingSource.DataSource = db.Contacts.Local.ToBindingList
+
+
+    End Sub
 End Class
